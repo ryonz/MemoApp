aa1efb04d1d346a76f4f511a61fed48540513679
Ryo Okube <ryo_okube@RyonoMacBook-puro.local>
2018-08-29 20:51:44 -0700

    Checking Problem

--- src/elements/CircleButton.js
+++ src/elements/CircleButton.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Font } from 'expo';
 import { StyleSheet, View, Text } from 'react-native';
 
+import fontAwesome from '../../assets/fonts/fontawesome-webfont.ttf';
+
+
 class CircleButton extends React.Component {
+  state = {
+    fontLoaded: false,
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      FontAwesome: fontAwesome,
+    });
+    this.setState({ fontLoaded: true });
+  }
+
   render() {
     const { style, color } = this.props;
 
@@ -14,10 +29,14 @@ class CircleButton extends React.Component {
     }
 
     return (
-      <View style={[styles.circleButton, style, { backgroundColor: bgColor}]}>
-        <Text style={[styles.circleButtonTitle, { color: textColor}]}>
-          {this.props.children}
-        </Text>
+      <View style={[styles.circleButton, style, { backgroundColor: bgColor }]}>
+        {
+          this.state.fontLoaded ? (
+            <Text style={[styles.circleButtonTitle, { color: textColor }]}>
+              {this.props.children}
+            </Text>
+          ) : null
+        }
       </View>
     );
   }
@@ -25,6 +44,7 @@ class CircleButton extends React.Component {
 
 const styles = StyleSheet.create({
   circleButton: {
+    fontFamily: 'FontAwesome',
     position: 'absolute',
     bottom: 32,
     right: 32,
@@ -34,7 +54,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     shadowColor: '#000',
-    shadowOffset: { widht: 0, height: 2},
+    shadowOffset: { widht: 0, height: 2 },
     shadowOpacity: 0.5,
     shadowRadius: 3,
   },
